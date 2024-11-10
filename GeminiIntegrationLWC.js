97d0f5c6a1d471f96fd46688d3ea45cc56d55fa2
Chino <88289548+crespoc@users.noreply.github.com>
2024-11-10 18:41:30 -0300

    Create GeminiIntegrationLWC.js

---
+++ GeminiIntegrationLWC.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+import makePostRequestGemini from '@salesforce/apex/GeminiCallouts.makePostRequest';
+
+export default class GeminiIntegrationLWC extends LightningElement {
+    
+    responsePresent = false
+    showSpinner = false
+
+    async handleClick() {
+        console.log('Hello val>'+this.refs.inputFromUser.value)
+
+        try {
+            this.showSpinner = true
+            const response = await makePostRequestGemini({inputFromUser: this.refs.inputFromUser.value});
+            this.responsePresent = true
+
+            this.handleResponseGemini(response)
+        } catch(ex) {
+            console.log(ex.message)
+        } finally {
+            this.showSpinner = false
+        }
+    }
+    handleResponseGemini(resp) {
+        let parsedResponse = JSON.parse(resp)
+        setTimeout(()=>{
+            console.log('Hello val>'+parsedResponse)
+            this.refs.textAreaResult.value = parsedResponse.candidates[0].content.parts[0].text
+        },1000)
+    }
+}
